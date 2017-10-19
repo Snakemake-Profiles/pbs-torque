7540d5073e459e1fb3c8215a83132fca8d8159bc
Neil Venables <neil.venables@cruk.manchester.ac.uk>
2017-10-19 08:05:08 +0100

    PBS support for hold and release.

--- {{cookiecutter.profile_name}}/pbs-jobscript.sh
+++ {{cookiecutter.profile_name}}/pbs-jobscript.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 # properties = {properties}
 {assignments}
-module load ${{SM_params_mod}} > /dev/null 2>&1
 {exec_job}
--- {{cookiecutter.profile_name}}/pbs-status.py
+++ {{cookiecutter.profile_name}}/pbs-status.py
@@ -7,19 +7,19 @@ import xml.etree.cElementTree as ET
 jobid = sys.argv[1]
 
 try:
-    res = subprocess.run("qstat -x {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
-except subprocess.CalledProcessError as e:
-    print("status-cmd error: ", e.stdout, file=sys.stderr)
-    raise e
+    res = subprocess.run("qstat -f -x {}".format(jobid), check=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True)
 
-xmldoc = ET.ElementTree(ET.fromstring(res.stdout.decode())).getroot()
-job_state = xmldoc.findall('.//job_state')[0].text
+    xmldoc = ET.ElementTree(ET.fromstring(res.stdout.decode())).getroot()
+    job_state = xmldoc.findall('.//job_state')[0].text
 
-if job_state == "C":
-    exit_status = xmldoc.findall('.//exit_status')[0].text
-    if exit_status == '0':
-        print("success")
+    if job_state == "C":
+        exit_status = xmldoc.findall('.//exit_status')[0].text
+        if exit_status == '0':
+            print("success")
+        else:
+            print("failed")
     else:
-        print("failed")
-else:
-    print("running")
+        print("running")
+
+except subprocess.CalledProcessError as e:
+    print("failed")
